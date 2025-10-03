fc84e76e74c0a9cff3abd9b722f80efbf7624a52
Vladimir S. Medvedev <stalker1984@mail.ru>
2025-10-03 10:10:57 +0300

    Update component.js

--- component.js
+++ component.js
@@ -1,5 +1,16 @@
 function MyButton() {
   return (
-    <button>Я кнопка</button>
+    <button>
+      Я кнопка
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Добро пожаловать в моё приложение</h1>
+      <MyButton />
+    </div>
   );
 }
